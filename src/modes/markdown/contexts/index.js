f8dfb98062f04f8a6dda7e74e83f095016aace23
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2017-07-24 13:35:43 -0700

    Remove Non-contexts from Context Lists

--- src/modes/markdown/contexts/index.js
+++ src/modes/markdown/contexts/index.js
@@ -1,22 +1,13 @@
 'use strict';
 
-var boldOrItalic = require('./boldOrItalic');
-var linkOrImageOrAttachment = require('./linkOrImageOrAttachment');
 var blockquote = require('./blockquote');
 var codeblock = require('./codeblock');
 var heading = require('./heading');
 var list = require('./list');
-var hr = require('./hr');
 
 module.exports = {
-  attachment: linkOrImageOrAttachment,
   blockquote: blockquote,
-  bold: boldOrItalic,
   code: codeblock,
   heading: heading,
-  hr: hr,
-  image: linkOrImageOrAttachment,
-  italic: boldOrItalic,
-  link: linkOrImageOrAttachment,
   list: list,
 };
