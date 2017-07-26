8866fa59e7bb76d94b5d9a3f371b93a45f56e998
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2017-07-26 10:12:02 -0700

    Test Fixes
    In prep for adding more tests, we need to keep the current ones working

--- test/unit/renderers.spec.js
+++ test/unit/renderers.spec.js
@@ -21,13 +21,13 @@ describe('renderers', function () {
     it('should set the text from any localizable strings if the ID is a match', function () {
       var el = {};
       renderers.modes(el, 'baz');
-      expect(el.text).toBe('bin');
+      expect(el.textContent).toBe('bin');
     });
 
     it('should use the ID as fallback text if localized text is not available', function () {
       var el = {};
       renderers.modes(el, 'quux');
-      expect(el.text).toBe('quux');
+      expect(el.textContent).toBe('quux');
     });
   });
 
@@ -36,13 +36,13 @@ describe('renderers', function () {
     it('should set the text from any localizable strings if the ID is a match', function () {
       var el = {};
       renderers.commands(el, 'foo');
-      expect(el.text).toBe('bar');
+      expect(el.textContent).toBe('bar');
     });
 
     it('should use the ID as fallback text if localized text is not available', function () {
       var el = {};
       renderers.commands(el, 'quux');
-      expect(el.text).toBe('quux');
+      expect(el.textContent).toBe('quux');
     });
   });
 });
