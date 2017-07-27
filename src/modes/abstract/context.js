1d3104a371c9c748f7ccb1395c343426c73ee40c
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2017-07-27 11:16:18 -0700

    Simplify Abstract `wrap`/`unwrap` (Don't Assume HTML)

--- src/modes/abstract/context.js
+++ src/modes/abstract/context.js
@@ -1,23 +1,17 @@
 'use strict';
 
-var doc = global.document;
-
 function Context (mode, editor, options) {
   this.mode = mode;
   this.editor = editor;
   this.options = options || {};
 }
 
-Context.prototype.wrap = function (nodes) {
-  var div = doc.createElement('div');
-  for(var c = 0, l = nodes.length; c < l; c++) {
-    div.appendChild(nodes[c]);
-  }
-  return div;
+Context.prototype.wrap = function () {
+  return '';
 };
 
-Context.prototype.unwrap = function (node) {
-  return node.childNodes;
+Context.prototype.unwrap = function () {
+  return '';
 };
 
 Context.prototype.isActive = function () {
